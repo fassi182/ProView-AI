ef0640e07de4484fd785921f5ba0e1645ac9f9b2
Muhammad Fassi Ur  Rehman <fassirehman07@gmail.com>
2026-04-19 16:29:37 +0500

    Updated project

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 #app/config.py
 import os
 from dotenv import load_dotenv
-
+import streamlit as st
 load_dotenv()
 
 class ProViewConfig:
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_API_KEY = st.secrets.get("GROQ_API_KEY") or os.getenv("GROQ_API_KEY")
 
     MODEL_NAME = "llama-3.3-70b-versatile"
     TEMPERATURE = 0.7
